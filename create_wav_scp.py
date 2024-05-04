c27c980bfda72e2e16d5e76490d3ff71848a34cb
vassdel <v.delhs@gmail.com>
2024-05-04 23:31:44 +0300

    4 completed

--- create_wav_scp.py
+++ create_wav_scp.py
@@ -2,7 +2,7 @@ def format_uttids(input_file, output_file):
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         for line in infile:
             id = line.strip()  # Remove newline and any extra spaces
-            formatted_line = f"~/kaldi/egs/usc/wav/{id}.wav\n"
+            formatted_line = f"wav/{id}.wav\n"
             outfile.write(id + ' ' + formatted_line)
 
 
